2586365d27d0585c9e2f06f69d79e4f1ddf4abe0
Blender <tommaso.piazza@gmail.com>
2012-03-08 12:11:52 +0100

    Stes to support device-device discovery Added CodeineMessage to define query messeges to the Codeine Application Added DeviceInformation to define the pair descriptorByteValue, ipAddress Added PackedDeviceInformation for ease.

---
+++ Codeine/DeviceInformation.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codeine
+{
+    public class DeviceInformation
+    {
+        public byte contactDescriptorByteValue;
+        public string ipAddr;
+
+        public DeviceInformation(byte cdByteValue, string ipAddr) 
+        {
+            this.contactDescriptorByteValue = cdByteValue;
+            this.ipAddr = ipAddr;
+        }
+
+        public byte[] ToArray() 
+        {
+            MemoryStream ms = new MemoryStream();
+            ms.Write(BitConverter.GetBytes(this.contactDescriptorByteValue), 0, sizeof(byte));
+            byte[] bytes = Encoding.ASCII.GetBytes(this.ipAddr);
+            ms.Write(bytes, 0, bytes.Length);
+
+            return ms.ToArray();
+        }
+    }
+}
---
+++ Codeine/PackedDeviceInformations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codeine
+{
+    public class PackedDeviceInformations
+    {
+        public DeviceInformation[] devices;
+        public byte count;
+
+        public PackedDeviceInformations(byte count, DeviceInformation[] deviceInfos)
+        {
+            this.devices = deviceInfos;
+            this.count = count;
+        }
+
+        public byte[] ToArray() 
+        {
+            MemoryStream ms = new MemoryStream();
+            ms.Write(BitConverter.GetBytes(this.count), 0, sizeof(byte));
+            for (int i = 0; i < devices.Length; i++) 
+            {
+                byte[] bytes = devices[i].ToArray();
+                ms.Write(bytes, 0, bytes.Length);
+            }
+
+            return ms.ToArray();
+        }
+    }
+}
