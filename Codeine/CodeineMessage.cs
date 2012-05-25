c39facb3d7fcc72a7d6aafd663f24ae70c701f90
Blender <tommaso.piazza@gmail.com>
2012-05-25 15:58:18 +0200

    GPL Header Added

--- Codeine/CodeineMessage.cs
+++ Codeine/CodeineMessage.cs
@@ -1,4 +1,21 @@
-﻿using System.Runtime.InteropServices;
+﻿//  Copyright (c) 2012 Tommaso Piazza <tommaso.piazza@gmail.com>
+//
+//  This file is part of Codeine.
+//
+//  Codeine is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Codeine is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Codeine.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Runtime.InteropServices;
 using System;
 using System.IO;
 
--- CodeineEmulator/EmulatorServerController.cs
+++ CodeineEmulator/EmulatorServerController.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿//  Copyright (c) 2012 Tommaso Piazza <tommaso.piazza@gmail.com>
+//
+//  This file is part of CodeineEmulator.
+//
+//  CodeineEmulator is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  CodeineEmulator is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with CodeineEmulator.  If not, see <http://www.gnu.org/licenses/>.
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
